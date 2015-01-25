38dc8b06a8e02255912c5758b8860ddcbcb92100
Revathy L M <revathymanulal@hotmail.com>
2015-01-26 00:28:50 +0530

    done

--- mm.c
+++ mm.c
@@ -1,25 +1,6 @@
 #include <stdio.h>      
 #include <stdlib.h>     
 
-int t,n;
-scanf("%d",&t);
-scanf("%d",&n);
-
-int boys[n], girls[n];
-
-for (int i=0;i<n;i++)
-{
-	cin>>girls[i];
-	
-}
-
-for (int i=0;i<n;i++)
-{
-	cin>>boys[i];
-	
-}
-
-
 
 
 
@@ -30,14 +11,63 @@ int compare (const void * a, const void * b)
 
 int main ()
 {
+int t, i,j,k N, A[100000], B[100000], c[1000];
+    scanf("%d",&t);
+    if(t>=1)
+    {
+if(t<=100)
+{
+  scanf("%d",&N);
+  if(N>=1)
+  {
+    if(N<=10000)
+    {for( j=0;j<t;j++)
+    {
+      for( i=0;i<N;i++)
+      {
+        scanf("%d",&A[i]);
+      if(A[i]!>=1)
+       exit(0);
+      }
+      for( i=0;i<N;i++)
+      {
+        scanf("%d",&B[i]);
+      if(B[i]!<=100000)
+       exit(0);
+      }
+  qsort (A, n, sizeof(int), compare);
+  //for (k=0; k<N; N++)
+    // printf ("%d\n ",A[N]);
+ qsort (B, n, sizeof(int), compare);
+  //for (k=0; k<N; N++)
+    // printf ("%d\n ",B[n]);for
+ int count=0;
+ for(i=0;i<N;i++)
+ {
+  if(B[N-i]%A[i]==0)
+  {
+    count++;
+  }
+  if(A[i]%B[N-i]==0)
+  {
+    count++;
+  }
+ }
+ c[j]=count;
+    }
+    }
+    for (int i = 0; i < t; ++i)
+    {
+      /* code */
+    
+    printf("d\n",c[j]);
+  }
+  }
+  
+}
+  }
 
-  int k;
-  qsort (boys, n, sizeof(int), compare);
-  for (k=0; k<6; n++)
-     printf ("%d\n ",boys[n]);
- qsort (girls, n, sizeof(int), compare);
-  for (k=0; k<6; n++)
-     printf ("%d\n ",girls[n]);
+  
   return 0;
 }
 
